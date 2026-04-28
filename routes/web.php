22f9e1479e4b83534817e4508b5576a8234caa5d
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-28 12:21:24 -0700

    initial calloff app

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'App is working';
+});
+
+// This will be your Twilio webhook
+Route::post('/calloff', function () {
+    $message = request('Body');
+    $from = request('From');
+
+    \Log::info('Call-off message received', [
+        'from' => $from,
+        'message' => $message
+    ]);
+
+    return response('OK');
 });

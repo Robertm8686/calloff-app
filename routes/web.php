91c737775481cecff2fa0bcda5b42c51bf515165
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-28 23:22:48 -0700

    fix twilio response syntax

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return 'App is working';
 });
 
 Route::post('/sms', function (Request $request) {
-    Log::info('Call off message received', [
-        'from' => $request->input('From'),
-        'message' => $request->input('Body'),
-    ]);
+    $message = $request->input('Body');
 
-    return response('<?xml version="1.0" encoding="UTF-8"?><Response><Message>Received your message</Message></Response>', 200)
-        ->header('Content-Type', 'text/xml');
+    return response(
+        '<Response><Message>Got your message: '.$message.'</Message></Response>',
+        200
+    )->header('Content-Type', 'text/xml');
 });

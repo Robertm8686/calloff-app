933b90248fff2ecd59b30ee578ddd2b8e51a72c5
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-28 19:43:38 -0700

    fix sms route

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return 'App is working';
 });
 
-Route::get('/public', function () {
-    return 'App is working';
-});
-
-Route::post('/calloff', function () {
+Route::post('/sms', function () {
     $message = request('Body');
     $from = request('From');
 
-    \Log::info('Call-off message received', [
+    Log::info('SMS received', [
         'from' => $from,
         'message' => $message
     ]);
 
     return response('OK');
-});
-
-Route::fallback(function () {
-    return 'App is working';
 });

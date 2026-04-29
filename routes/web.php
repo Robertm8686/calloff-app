ed87b305f248ab1967c8700d02e40284bbe8c2b8
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-29 11:37:56 -0700

    final routes with status dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,35 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    return 'App is working';
-});
+/*
+|--------------------------------------------------------------------------
+| SMS Webhook (Twilio)
+|--------------------------------------------------------------------------
+*/
 
 Route::post('/sms', function (Request $request) {
+    $message = strtolower($request->input('Body'));
+    $from = $request->input('From');
+
+    // Detect call off
+    $status = str_contains($message, 'call off') ? 'CALLOFF' : 'OTHER';
+
     DB::table('messages')->insert([
-        'from' => $request->input('From'),
-        'body' => $request->input('Body'),
+        'from' => $from,
+        'body' => $message,
+        'status' => $status,
         'created_at' => now(),
         'updated_at' => now(),
     ]);
 
     return response('', 200);
 });
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard (View Messages)
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/messages', function () {
     $messages = DB::table('messages')
@@ -26,13 +41,14 @@ Route::get('/messages', function () {
 
     $html = '<h1>Calloff Messages</h1>';
     $html .= '<table border="1" cellpadding="10">';
-    $html .= '<tr><th>ID</th><th>From</th><th>Message</th><th>Received At</th></tr>';
+    $html .= '<tr><th>ID</th><th>From</th><th>Message</th><th>Status</th><th>Received At</th></tr>';
 
     foreach ($messages as $message) {
         $html .= '<tr>';
         $html .= '<td>'.$message->id.'</td>';
         $html .= '<td>'.$message->from.'</td>';
         $html .= '<td>'.$message->body.'</td>';
+        $html .= '<td>'.$message->status.'</td>';
         $html .= '<td>'.$message->created_at.'</td>';
         $html .= '</tr>';
     }
@@ -40,4 +56,14 @@ Route::get('/messages', function () {
     $html .= '</table>';
 
     return $html;
+});
+
+/*
+|--------------------------------------------------------------------------
+| Root Test
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+    return 'App is running';
 });

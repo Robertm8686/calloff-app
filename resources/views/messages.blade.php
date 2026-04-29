2da2f22f993d19552ffefefaba4ea36de4f83f4a
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-29 16:05:13 -0700

    add messages blade view

---
+++ resources/views/messages.blade.php
@@ -0,0 +1,21 @@
+<h1>Calloff Messages</h1>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>ID</th>
+        <th>From</th>
+        <th>Message</th>
+        <th>Status</th>
+        <th>Received At</th>
+    </tr>
+
+    <?php foreach ($messages as $msg): ?>
+    <tr>
+        <td><?= $msg->id ?></td>
+        <td><?= $msg->from ?></td>
+        <td><?= $msg->body ?></td>
+        <td><?= $msg->status ?? 'N/A' ?></td>
+        <td><?= $msg->created_at ?></td>
+    </tr>
+    <?php endforeach; ?>
+</table>

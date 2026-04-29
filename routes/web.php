0760c145121e188a6b7646a5b8e0e252f88cfaab
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-29 12:33:54 -0700

    final working routes

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
-| SMS Webhook (Twilio)
+| Web Routes
 |--------------------------------------------------------------------------
 */
 
+// 🔹 Receive SMS (Twilio webhook)
 Route::post('/sms', function (Request $request) {
+
     $message = strtolower($request->input('Body'));
     $from = $request->input('From');
 
@@ -28,42 +30,9 @@ Route::post('/sms', function (Request $request) {
     return response('', 200);
 });
 
-/*
-|--------------------------------------------------------------------------
-| Dashboard (View Messages)
-|--------------------------------------------------------------------------
-*/
 
+// 🔹 Dashboard page
 Route::get('/messages', function () {
-    $messages = DB::table('messages')
-        ->orderByDesc('created_at')
-        ->get();
-
-    $html = '<h1>Calloff Messages</h1>';
-    $html .= '<table border="1" cellpadding="10">';
-    $html .= '<tr><th>ID</th><th>From</th><th>Message</th><th>Status</th><th>Received At</th></tr>';
-
-    foreach ($messages as $message) {
-        $html .= '<tr>';
-        $html .= '<td>'.$message->id.'</td>';
-        $html .= '<td>'.$message->from.'</td>';
-        $html .= '<td>'.$message->body.'</td>';
-        $html .= '<td>'.$message->status.'</td>';
-        $html .= '<td>'.$message->created_at.'</td>';
-        $html .= '</tr>';
-    }
-
-    $html .= '</table>';
-
-    return $html;
-});
-
-/*
-|--------------------------------------------------------------------------
-| Root Test
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/', function () {
-    return 'App is running';
+    $messages = DB::table('messages')->get();
+    return view('messages', ['messages' => $messages]);
 });

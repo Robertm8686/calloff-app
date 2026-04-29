246bb2d0f210e4cf8e867b08901a5593eb68b7ce
Robert Morinaga <robert.m@essentialstaff.com>
2026-04-29 09:08:56 -0700

    save incoming sms to database

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-
-Route::get('/', function () {
-    return 'App is working';
-});
+use Illuminate\Support\Facades\DB;
 
 Route::post('/sms', function (Request $request) {
     $message = $request->input('Body');
+    $from = $request->input('From');
 
-    // temporarily disable reply
+    // Save to database
+    DB::table('messages')->insert([
+        'from' => $from,
+        'body' => $message,
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
     return response('', 200);
 });

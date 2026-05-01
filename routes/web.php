33254ec1672f37faa4dbd866bcaed792eb2313c5
Robert Morinaga <robert.m@essentialstaff.com>
2026-05-01 10:25:13 -0700

    fix routes file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
-Route::post('/sms', function (Request $request) {
+<?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+Route::post('/sms', function (Request $request) {
     $message = strtolower($request->input('Body'));
     $from = $request->input('From');
 
@@ -13,13 +19,26 @@ Route::post('/sms', function (Request $request) {
         'updated_at' => now(),
     ]);
 
-    // 🔥 ADD THIS
     if ($status === 'CALLOFF') {
-        Mail::raw('Employee called off: ' . $message, function ($mail) {
+        Mail::raw("Employee called off.\n\nFrom: $from\nMessage: $message", function ($mail) {
             $mail->to('kenji26m@gmail.com')
-                 ->subject('Call Off Alert');
+                ->subject('Call Off Alert');
         });
     }
 
     return response('', 200);
+});
+
+Route::get('/messages', function () {
+    $messages = DB::table('messages')->get();
+    return view('messages', ['messages' => $messages]);
+});
+
+Route::get('/test-email', function () {
+    Mail::raw('This is a test email from the Calloff App.', function ($message) {
+        $message->to('kenji26m@gmail.com')
+            ->subject('Calloff App Test Email');
+    });
+
+    return 'Email sent';
 });
